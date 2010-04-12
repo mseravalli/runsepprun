f278b31e8eec3f4ade23f582bd8a4f1d3f61555c
corneliu.ilisescu <corneliu.ilisescu@8b988cb1-803b-4bfd-f238-c3485f5ddb4c>
2010-04-12 16:22:10 +0000

    das

---
+++ RunSeppRun/Character.cpp
@@ -0,0 +1,14 @@
+/* 
+ * File:   Character.cpp
+ * Author: korn
+ * 
+ * Created on 12 aprile 2010, 18.16
+ */
+
+#include "Character.h"
+
+Character::Character() {
+}
+
+
+
---
+++ RunSeppRun/Player.cpp
@@ -0,0 +1,11 @@
+/* 
+ * File:   Player.cpp
+ * Author: korn
+ * 
+ * Created on 12 aprile 2010, 18.19
+ */
+
+#include "Player.h"
+
+Player::Player() {
+}
