873c67ac794e89bf04776dd48c487a55fc345153
corneliu.ilisescu <corneliu.ilisescu@8b988cb1-803b-4bfd-f238-c3485f5ddb4c>
2010-04-12 16:21:52 +0000

    das

---
+++ trunk/RunSeppRun/Player.h
@@ -0,0 +1,22 @@
+/* 
+ * File:   Player.h
+ * Author: korn
+ *
+ * Created on 12 aprile 2010, 18.19
+ */
+
+#ifndef _PLAYER_H
+#define	_PLAYER_H
+
+class Player {
+public:
+    Player();
+    void move();
+    void hit();
+    void launch();
+private:
+
+};
+
+#endif	/* _PLAYER_H */
+
